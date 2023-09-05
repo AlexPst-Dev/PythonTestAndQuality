ab275c1b0719da72578a27350c25a954b5eec064
Nicolas <arnaud.nicolas@supdevinci-edu.fr>
2023-09-05 15:41:45 +0200

    Ajout hashage

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,12 +1,17 @@
 import string
 import random
+import hashlib
 
 lowercase_letters = string.ascii_lowercase
 uppercase_letters = string.ascii_uppercase
 digits = string.digits
 symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
 
-def generate_password(length, use_lowercase=True, use_uppercase=True, use_digits=True, use_symbols=True):
+hasherSHA256 = hashlib.sha256() # SHA-256
+hasherMD5 = hashlib.md5() # MD5
+
+
+def generate_password(length, use_lowercase=True, use_uppercase=True, use_digits=True, use_symbols=True, use_hash="none"):
     characters = ""
     if use_lowercase:
         characters += lowercase_letters
@@ -21,7 +26,18 @@ def generate_password(length, use_lowercase=True, use_uppercase=True, use_digits
         raise ValueError("No characters to choose from!")
     
     password = "".join(random.choice(characters) for _ in range(length))
-    return password
+    if use_hash == "SHA-256":
+        password = password.encode("utf-8")
+        hasherSHA256.update(password)
+        hashed_password = hasherSHA256.hexdigest()
+        return hashed_password
+    elif use_hash == "MD5":
+        password = password.encode("utf-8")
+        hasherMD5.update(password)
+        hashed_password = hasherMD5.hexdigest()
+        return hashed_password
+    else:
+        return password
 
 if __name__ == "__main__":
-    print(generate_password(12))
+    print(generate_password(12, True, True, True, True, "SHA-256"))
