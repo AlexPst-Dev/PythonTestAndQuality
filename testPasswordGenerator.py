bd4ff2330c55f0b9ac3a1a4fec87a90f6e556202
Nicolas <arnaud.nicolas@supdevinci-edu.fr>
2023-09-07 09:35:08 +0200

    Ajout tests

--- testPasswordGenerator.py
+++ testPasswordGenerator.py
@@ -5,43 +5,63 @@ class TestPasswordGenerator(unittest.TestCase):
     
     def test_generate_password_length(self):
         # Vérifiez que la longueur du mot de passe généré est correcte
-        length = 12
-        password = passwordGenerator.generate_password(length)
-        self.assertEqual(len(password), length)
+        for length in range(5, 25):
+            password = passwordGenerator.generate_password(length)
+            self.assertEqual(len(password), length)
+
+    def test_generate_password_length_under_five(self):
+        # Vérifiez qu'une erreur est levée si la longueur du mot de passe est inférieure à 5
+        with self.assertRaises(ValueError):
+            passwordGenerator.generate_password(4)
+
+    def test_generate_password_length_over_twenty_five(self):
+        # Vérifiez qu'une erreur est levée si la longueur du mot de passe est supérieure à 25
+        with self.assertRaises(ValueError):
+            passwordGenerator.generate_password(26)
+
+    def test_generate_password_length_negative(self):
+        # Vérifiez qu'une erreur est levée si la longueur du mot de passe est négative
+        with self.assertRaises(ValueError):
+            passwordGenerator.generate_password(-1)
+
+    def test_generate_password_length_null(self):
+        # Vérifiez qu'une erreur est levée si la longueur du mot de passe est nulle
+        with self.assertRaises(ValueError):
+            passwordGenerator.generate_password(0)
 
     def test_generate_password_lowercase(self):
         # Vérifiez que le mot de passe contient des lettres minuscules
-        password = passwordGenerator.generate_password(8, use_lowercase=True, use_uppercase=False)
+        password = passwordGenerator.generate_password(25, use_lowercase=True, use_uppercase=False)
         self.assertTrue(any(c.islower() for c in password))
 
     def test_generate_password_uppercase(self):
         # Vérifiez que le mot de passe contient des lettres majuscules
-        password = passwordGenerator.generate_password(8, use_lowercase=False, use_uppercase=True)
+        password = passwordGenerator.generate_password(25, use_lowercase=False, use_uppercase=True)
         self.assertTrue(any(c.isupper() for c in password))
 
     def test_generate_password_digits(self):
         # Vérifiez que le mot de passe contient des chiffres
-        password = passwordGenerator.generate_password(8, use_digits=True, use_symbols=False)
+        password = passwordGenerator.generate_password(25, use_digits=True, use_symbols=False)
         self.assertTrue(any(c.isdigit() for c in password))
 
     def test_generate_password_symbols(self):
         # Vérifiez que le mot de passe contient des symboles
-        password = passwordGenerator.generate_password(8, use_symbols=True, use_lowercase=False)
+        password = passwordGenerator.generate_password(25, use_symbols=True, use_lowercase=False)
         self.assertTrue(any(c in "!@#$%^&*()_+-=[]{}|;:,.<>?/" for c in password))
 
     def test_generate_password_no_character_selected(self):
         # Vérifiez qu'une erreur est levée si aucun caractère n'est sélectionné
         with self.assertRaises(ValueError):
-            passwordGenerator.generate_password(8, use_lowercase=False, use_uppercase=False, use_digits=False, use_symbols=False)
+            passwordGenerator.generate_password(25, use_lowercase=False, use_uppercase=False, use_digits=False, use_symbols=False)
 
     def test_generate_password_hash_sha256(self):
         # Vérifiez que le mot de passe généré est correctement haché
-        password = passwordGenerator.generate_password(8, use_hash="SHA-256")
+        password = passwordGenerator.generate_password(25, use_hash="SHA-256")
         self.assertEqual(len(password), 64)
     
     def test_generate_password_hash_md5(self):
         # Vérifiez que le mot de passe généré est correctement haché
-        password = passwordGenerator.generate_password(8, use_hash="MD5")
+        password = passwordGenerator.generate_password(25, use_hash="MD5")
         self.assertEqual(len(password), 32)
 
 if __name__ == '__main__':
